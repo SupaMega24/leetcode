db342a63714ba73af5193aa345a002f19b241cfa
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-03-11 21:58:56 +0900

    Add solution

---
+++ 30DaysOfJavaScript/2635-applyTransfromEachArrayElement.js
@@ -0,0 +1,18 @@
+// Given an integer array arr and a mapping function fn, 
+// return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+// MY SOLUTION  
+
+const map = (arr, fn) => {
+    const transformedArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        transformedArray[i] = fn(arr[i], i);
+    }
+
+    return transformedArray;
+};
